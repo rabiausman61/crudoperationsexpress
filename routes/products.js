8c0aa019c690e005647f2ac2ace105989e6bbf98
rabiausman61 <81038373+rabiausman61@users.noreply.github.com>
2021-06-15 00:33:44 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var product=require("../models/product");
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+/*router.get('/', async function(req, res, next) {
   try {
     let products= await product.find();
   res.render('products/list', {products});
-  } catch (error) {
+  } 
+  catch (error) {
     res.send("error");
   }
   
@@ -68,3 +69,4 @@ router.post("/edit/:id", async function (req, res, next) {
  
 });
 module.exports = router;
+*/
