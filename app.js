3387f7ef92fea383d52585e28e8d386ccdc75e1c
rabiausman61 <81038373+rabiausman61@users.noreply.github.com>
2021-06-14 23:46:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -39,8 +39,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-mongoose
-  .connect( "mongodb://localhost/productscrud", {
+mongoose.connect( "mongodb://localhost/productscrud", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
