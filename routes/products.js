356ece7ac8ac17e986ae8e0158d6d08123f4931e
rabiausman61 <81038373+rabiausman61@users.noreply.github.com>
2021-06-15 00:04:01 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -4,38 +4,67 @@ var product=require("../models/product");
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let products= await product.find();
+  try {
+    let products= await product.find();
   res.render('products/list', {products});
+  } catch (error) {
+    res.render(e);
+  }
+  
 });
 
 router.get('/add', async function(req, res, next) {
+  try{
   res.render('products/add');
+  }
+  catch(error){res.render(e);
+  }
 });
 
 router.post('/add', async function(req, res, next) {
-  let p= new product(req.body);
+  try {
+    let p= new product(req.body);
   await p.save();
   res.redirect("/products");
+  } catch (error) {
+    res.render(e);
+  }
+  
 });
 
 
 router.get('/delete/:id', async function(req, res, next) {
-  let p= await product.findByIdAndDelete(req.params.id);
+  try {
+    let p= await product.findByIdAndDelete(req.params.id);
   res.redirect("/products");
+  } catch (error) {
+    res.render(e);
+  }
+  
 });
 
 
 
 
 router.get("/edit/:id", async function (req, res, next) {
-  let products = await product.findById(req.params.id);
+  try {
+    let products = await product.findById(req.params.id);
   res.render("products/edit", { products });
+  } catch (error) {
+    res.render(e);
+  }
+  
 });
 router.post("/edit/:id", async function (req, res, next) {
-  let products = await product.findById(req.params.id);
-  products.name = req.body.name;
-  products.gender = req.body.gender;
-  await products.save();
-  res.redirect("/products");
+  try {
+    let products = await product.findById(req.params.id);
+    products.name = req.body.name;
+    products.gender = req.body.gender;
+    await products.save();
+    res.redirect("/products");
+  } catch (error) {
+    res.render(e);
+  }
+ 
 });
 module.exports = router;
